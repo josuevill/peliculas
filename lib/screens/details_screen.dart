d4621c33dd09a138659c85e049b1420b62502eb7
josuevill <josvill003@outlook.com>
2023-02-24 08:05:11 -0500

    projecto inicial

---
+++ lib/screens/details_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DetailsScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+     return Scaffold(
+      body: Container(
+        child: const Center(
+          child: Text('Pantalla Detalles'),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: const Center(
+          child: Text('Pantalla Inicial'),
+        ),
+      ),
+    );
+  }
+}
