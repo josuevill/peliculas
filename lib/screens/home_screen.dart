910ebc89177c10c4ea48509b13dc4e124f6d26b4
josuevill <josvill003@outlook.com>
2023-02-24 10:27:59 -0500

    MovieSlider agregado

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:peliculas/widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: const Center(
-          child: Text('Pantalla Inicial'),
+      appBar: AppBar(
+        title: const Text('En cartelera'),
+        elevation: 0,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search_outlined),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            // tarjetas principales
+            CardSwiper(),
+
+            // slider de peliculas
+            MovieSlider(),
+          ],
         ),
       ),
     );
